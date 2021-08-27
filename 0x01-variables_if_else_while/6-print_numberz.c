73acf41b4ba82a80b1b39a88ca9b6c10fe3a2499
root <Mathyasyazew@gmail.com>
2021-08-27 02:24:36 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nody;
+
+	for (nody = '0' ; nody <= '9' ; nody++)
+	{
+	putchar(nody);
+	}
+	putchar('\n');
+
+return (0);
+}
