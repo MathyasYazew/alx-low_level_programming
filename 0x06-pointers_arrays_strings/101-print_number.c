b1cf9726662a98a686093ed06661002c2b267653
root <Mathyasyazew@gmail.com>
2021-09-09 13:24:30 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,29 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: void
  */
-int main(void)
-{
-int n;
-int a[5];
-int *p;
 
-a[2] = 1024;
-p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+void print_number(int n)
+{
+int divisor = 1, i, resp;
+
+if (n < 0)
+{
+	_putchar('-');
+	n *= -1;
+}
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+
+}
+
 }
