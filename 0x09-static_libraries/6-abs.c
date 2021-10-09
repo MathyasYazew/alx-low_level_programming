9528a3dfe03cd644c5d68ebf3862950c672d75c6
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2021-10-09 06:42:47 +0300

    UPDATE
    UPDATE

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function the absolute value.
+ * @numero: value absolute
+ *
+ * Return: n
+ */
+int _abs(int numero)
+{
+if (numero < 0)
+{
+numero *= -1;
+return (numero);
+}
+else
+{
+return (numero);
+}
+}
