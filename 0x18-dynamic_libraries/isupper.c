50b01847803b6b5d4b6b8f91dbe2db47a9a79966
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2021-12-29 15:18:47 +0300

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - prints one or zero depending on input
+ * @c: input type variable
+ *
+ * Description: checks for uppercase letters
+ * Return: Aways (0).
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
